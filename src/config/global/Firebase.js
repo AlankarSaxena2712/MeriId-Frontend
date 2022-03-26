c441d7a6ab13e1d9e8e956bd2de39a2cc5e9d7ad
Alankar Saxena <saxenaalankar42@gmail.com>
2022-03-26 11:02:31 +0530

    firebase configuration

---
+++ src/config/global/Firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA9zALcF_MMSo01g2_ep2blR4d6wZDLoH0",
+  authDomain: "meriid.firebaseapp.com",
+  projectId: "meriid",
+  storageBucket: "meriid.appspot.com",
+  messagingSenderId: "812001539717",
+  appId: "1:812001539717:web:21ce94a3414b1fbfb5dbec"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
