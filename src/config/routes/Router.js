cb862d74f4b345a83757445eb972526cf6c3fc92
Caesarking-Source <coder2880@gmail.com>
2022-08-04 00:20:31 +0530

    2nd day commit

--- src/config/routes/Router.js
+++ src/config/routes/Router.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Admin from "../../pages/Admin";
 import Home from "../../pages/Home";
+import Chat from "../../pages/Chat";
 
 const Router = () => {
 	return (
@@ -10,6 +11,7 @@ const Router = () => {
                 <Route path="/" element={<Home />} />
                 <Route path="/admin/*" element={<Admin />} />
                 <Route path="/*" element={<h1 className="flex justify-center items-center text-4xl font-bold h-full">Not Found</h1>} />
+				<Route path="/" element={<Chat />} />
             </Routes>
 		</BrowserRouter>
 	);
---
+++ src/pages/Chat.jsx
@@ -0,0 +1,46 @@
+import React, {Component} from "react";
+
+class KommunicateChat extends Component {
+
+   constructor(props){
+
+       super(props);
+
+   } 
+
+componentDidMount(){
+
+   (function(d, m){
+
+       var kommunicateSettings =
+      {"appId": "259ee76a76674e8ee1a6d02613a91595f", 
+      "popupWidget": true, 
+      "automaticChatOpenOnNavigation": true };
+
+       var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
+       s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+       var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
+
+       window.kommunicate = m; m._globals = kommunicateSettings;
+
+   })(document, window.kommunicate || {});
+
+}
+
+render(){
+
+    return(
+ 
+        <div>
+ 
+            <h1>Hello</h1>
+ 
+      </div>
+ 
+    )
+ 
+ }
+
+}
+
+export default KommunicateChat;
