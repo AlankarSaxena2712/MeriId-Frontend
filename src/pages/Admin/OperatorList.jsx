95b09b17d99e87176b52c80982ddca78576ec560
Caesarking-Source <coder2880@gmail.com>
2022-08-04 19:05:04 +0530

    3rd day

--- src/pages/Admin/OperatorList.jsx
+++ src/pages/Admin/OperatorList.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
+import ReCAPTCHA from 'react-google-recaptcha';
 
 const OperatorList = () => {
+
+  function onChange(value) {
+    console.log("Captcha value:", value);
+  }
+  
   return <div className='pt-16 px-32 w-full'>
     <h1 className="text-3xl font-bold p-4 text-center">Sign In</h1>
   <div class="w-full max-w-md ">
@@ -18,6 +24,11 @@ const OperatorList = () => {
       <input class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************" />
       <p class="text-red-500 text-xs italic">Please choose a password.</p>
     </div>
+    <div class="flex items-center justify-between mb-3">
+      <ReCAPTCHA 
+      sitekey='6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI'
+      onChange={onChange} />
+      </div>
     <div class="flex items-center justify-between">
       <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
         Sign In
